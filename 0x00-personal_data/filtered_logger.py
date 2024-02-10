a6d1deb4044fd3fba6b17740ffc7f38be360a8e6
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2024-02-10 19:40:39 +0200

    task 4

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -47,6 +47,20 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     return connector
 
 
+def main() -> None:
+    """main function"""
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM users;')
+    users = cursor.fetchall()
+    logger = get_logger()
+    for user in users:
+        message = f"name={user[0]}; email={user[1]}; phone={user[2]}; " + \
+            f"ssn={user[3]}; password={user[4]}; ip={user[5]}; " + \
+            f"last_login={user[6]}; user_agent={user[7]};"
+        logger.info(message)
+
+
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
         """
@@ -66,3 +80,7 @@ class RedactingFormatter(logging.Formatter):
             self.REDACTION,
             super().format(record),
             self.SEPARATOR)
+
+
+if __name__ == "__main__":
+    main()
