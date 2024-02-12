76db07a2ca1ce976b4877e51ade478a227f473e3
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2024-02-12 21:56:16 +0200

    task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -22,3 +23,21 @@ class BasicAuth(Auth):
         ):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+                                            self,
+                                            base64_authorization_header: str
+                                            ) -> str:
+        """returns the decoded value of a Base64 string"""
+        if (
+            base64_authorization_header is None
+            or type(base64_authorization_header) is not str
+        ):
+            return None
+        try:
+            base64_byte = base64.b64decode(
+                base64_authorization_header
+            )
+            return base64_byte.decode('utf-8')
+        except Exception:
+            return None
