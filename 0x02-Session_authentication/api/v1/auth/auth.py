764273b14edae4ff407713a11552a3e5d79ed561
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2024-02-14 21:58:37 +0200

    task 4

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -36,3 +36,9 @@ class Auth:
     def current_user(self, request=None):
         """current_user method"""
         return None
+
+    def session_cookie(self, request=None):
+        """returns a cookie value from a request"""
+        if request is None:
+            return None
+        return request.cookies.get("_my_session_id")
